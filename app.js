f3fe34339f63bbdf0a977fa80cd8f438a17de8d6
Harshvardhan Singh <136837729+Harsh-Singh-In@users.noreply.github.com>
2023-09-30 22:42:20 +0530

    Tic-Tac-Toe

---
+++ app.js
@@ -0,0 +1,99 @@
+const boxes = document.querySelectorAll(".box");
+const gameInfo = document.querySelector(".game-info");
+const newGameBtn = document.querySelector(".btn");
+let image = document.querySelector("#image");
+
+let currentPlayer;
+let gameGrid;
+
+const winningPositions = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+];
+
+function initGame() {
+    currentPlayer = "X";
+    gameGrid = ["","","","","","","","",""];
+    boxes.forEach((box,index) => {
+        box.innerText = "";
+        boxes[index].style.pointerEvents = "all";
+        box.classList = `box box${index+1}`;
+    });
+    newGameBtn.classList.remove("active");
+    image.classList.remove("image");
+    gameInfo.innerHTML = `Current Player - ${currentPlayer}`;
+}
+
+initGame();
+
+function swapTurn() {
+    if(currentPlayer==="X") {
+        currentPlayer = "O";
+    } else {
+        currentPlayer = "X";
+    }
+    gameInfo.innerText = `Current Player - ${currentPlayer}`; 
+}
+
+function checkGameOver() {
+    let result = "";
+    winningPositions.forEach((position) => {
+        if((gameGrid[position[0]] !== "" || gameGrid[position[1]] !== "" || gameGrid[position[2]] !=="") && (gameGrid[position[0]] === gameGrid[position[1]]) && (gameGrid[position[1]] === gameGrid[position[2]])) {
+            if(gameGrid[position[0]] === "X")
+                result = "X";
+            else
+                result = "O";
+
+            boxes.forEach((box) => {
+                box.style.pointerEvents = "none";
+            })
+
+            boxes[position[0]].classList.add("win");
+            boxes[position[1]].classList.add("win");
+            boxes[position[2]].classList.add("win");
+        }
+        
+    });
+
+    if (result !== "") {
+        gameInfo.innerText = `Winner Player - ${result}`;
+        image.classList.add("image");
+        newGameBtn.classList.add("active");
+        return;
+    }
+
+    let fillCount = 0;
+    gameGrid.forEach((box) => {
+        if(box!=="")
+            fillCount++;
+    });
+
+    if(fillCount===9){
+        gameInfo.innerText = "Game Tied !";
+        newGameBtn.classList.add("active");
+    }
+} 
+
+function handleClick(index) {
+    if (gameGrid[index] === "") {
+        boxes[index].innerText = currentPlayer;
+        gameGrid[index] = currentPlayer;
+        boxes[index].style.pointerEvents = "none";
+        swapTurn();
+        checkGameOver(); 
+    }
+}
+
+boxes.forEach((box,index) =>{
+    box.addEventListener("click", () => {
+        handleClick(index);
+    })
+});
+
+newGameBtn.addEventListener("click",initGame);
